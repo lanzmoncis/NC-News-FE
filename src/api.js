ec8fc03838690a5fb86bd2b0268fe06f9206d15f
lanzmoncis <lanzmoncis@gmail.com>
2023-03-14 16:30:57 +0000

    Update the api file

--- src/api.js
+++ src/api.js
@@ -11,15 +11,13 @@ export const getArticles = () => {
 };
 
 export const getSingleArticle = (articleId) => {
-  const parsedId = parseInt(articleId);
-  return articleApi.get(`/articles/${parsedId}`).then(({ data }) => {
+  return articleApi.get(`/articles/${articleId}`).then(({ data }) => {
     return data.articleById;
   });
 };
 
 export const getArticleComments = (articleId) => {
-  const parsedId = parseInt(articleId);
-  return articleApi.get(`/articles/${parsedId}/comments`).then(({ data }) => {
+  return articleApi.get(`/articles/${articleId}/comments`).then(({ data }) => {
     return data.commentsById;
   });
 };
